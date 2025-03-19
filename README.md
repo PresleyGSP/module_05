fe2028209a6e8f4f87f1a8ef6e19854408d9776d
Presley McFarlane-Goolcharan <pmcfarlanegoolcharan@rrc.ca>
2025-03-19 17:04:18 -0500

    Second commit: added account number validation

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ In this assignment I will demonstrate the use of defining functions and unit tes
 ## Author
 
 Presley McFarlane-Goolcharan
+
+## Code Modification [1]
+
+- Added account number validation to "get_account_number()" function.
--- src/chatbot.py
+++ src/chatbot.py
@@ -7,7 +7,7 @@ Example:
     $ python src/chatbot.py
 """
 
-__author__ = "COMP-1327 Faculty"
+__author__ = "Presley McFarlane-Goolcharan"
 __version__ = "1.0.2025"
 
 ACCOUNTS = {
@@ -25,8 +25,42 @@ VALID_TASKS = [
     "exit"
 ]
 
+def get_account_number() -> int:
+    """Prompts the user to input an account number, validates the input
+    and returns the account number as an integer.
+    
+    Returns:
+        int: Returns the account the number.
+    
+    Raises:
+        ValueError: Raised if account number entered cannot be parsed into an integer.
+        ValueError: Raised if the account number entered does not exist in the "ACCOUNTS" dictionary.
+
+    """
+    try:
+            acc_num = input(f"Please enter your account number: ")
+            try:
+                    iter = int(acc_num)
+            except ValueError:
+                    raise TypeError("Account must be a whole number.")
+            
+            if iter in ACCOUNTS:
+                result = print(iter)
+            else:
+                raise ValueError("Account number entered does not exist.")
+    except TypeError as e:
+            result = print(e)
+    except ValueError as e:
+            result = print(e)
+         
+    
+    return result
+
+get_account_number()
+    
+"""
 def chatbot():
-    """Performs the Chatbot functionality."""
+    Performs the Chatbot functionality.
     COMPANY_NAME = "PiXELL River Financial"
 
     # Print welcome message
@@ -38,3 +72,4 @@ def chatbot():
 
 if __name__ == "__main__":
     chatbot()
+"""
